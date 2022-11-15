cf14db83473f99e42d24675cfb242515fd33fdc4
Niloy <nahidniloy894@gmail.com>
2022-11-15 08:04:36 +0600

    15/11/22

--- Request Library/databaseConnect.py
+++ Request Library/databaseConnect.py
@@ -4,6 +4,6 @@ import mysql.connector
 
 conn = mysql.connector.connect(host='localhost', database='PythonAutomation', user='root', password='1414')
 print(conn.is_connected())  # to check connection
-cursor=conn.cursor()
-cursor.execute()
+# cursor=conn.cursor()
+# cursor.execute()
 
