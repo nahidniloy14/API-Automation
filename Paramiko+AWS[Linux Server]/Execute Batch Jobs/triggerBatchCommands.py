e127bef1f272e29d0f73b5bc8ba7627ff9c2b63b
Niloy <nahidniloy894@gmail.com>
2022-11-23 09:07:57 +0600

    23/11/22

--- Paramiko+AWS[Linux Server]/Execute Batch Jobs/triggerBatchCommands.py
+++ Paramiko+AWS[Linux Server]/Execute Batch Jobs/triggerBatchCommands.py
@@ -1,3 +1,5 @@
+import csv
+
 import paramiko as paramiko
 from Utilities.configuration import getConfig
 
@@ -14,15 +16,28 @@ ssh.connect(host, port, username, password)
 #--------Upload Files--------
 sftp=ssh.open_sftp()#this method is used to file transfer
 destinationPath="loan.csv"
-localPath="Utilities\loan.csv"
+localPath="Utilities/BatchJobsLoan.csv"
 sftp.put(localPath,destinationPath)#it will push the files in the aws environment
 
 sftp=ssh.open_sftp()#this method is used to file transfer
 destinationPath="script.py"
-localPath="Utilities\script.py"
+localPath="Utilities/script.py"
 sftp.put(localPath,destinationPath)#it will push the files in the aws environment
 
 
 #Trigger Batch Jobs
+
 stdin,stdout,stderr=ssh.exec_command("python script.py")#writing python is not mendatory if it is installed in your aws machine
-# print(stdout.readlines())
+print(stdout.readlines())
+
+#Download the file to loacl system
+sftp.get("loan.csv","Utilities/BatchJobsLoanUpdated.csv")
+
+#parse Output to CSV
+with open('Utilities/BatchJobsLoanUpdated.csv') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')  # name,status "," is delimiter
+    Name = []
+    Status = []
+    for row in csvReader:
+        if row[0]=="Antu":
+            assert row[1] == "PAID"
---
+++ Utilities/script.py
@@ -0,0 +1,1 @@
+#------------this section will beprovided by the developer-------------------
